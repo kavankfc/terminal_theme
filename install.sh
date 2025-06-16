8168cdb28ee53568bb2418c4f7ef108b98cd967d
Kavan-server <kavankfc@gmail.com>
2025-06-16 02:52:46 -0700

    include fzf-tab into zsh terminal tool

--- install.sh
+++ install.sh
@@ -16,8 +16,9 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 # install zsh plugins (zsh-autosuggestions, zsh-syntax-highlighting)
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
 # activate plugins
-sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/' ~/.zshrc
+sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting fzf-tab)/' ~/.zshrc
 
 # install starship
 curl -sS https://starship.rs/install.sh | sh -s -- --yes
